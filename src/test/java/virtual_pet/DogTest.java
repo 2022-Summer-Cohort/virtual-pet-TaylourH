c2f35963ee7587d168bff422ac49f8f48e27e817
TaylourH <harristaylour@gmail.com>
2022-06-01 22:14:09 -0400

    1st Iteration with Tests Done

--- src/test/java/virtual_pet/DogTest.java
+++ src/test/java/virtual_pet/DogTest.java
@@ -7,6 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class DogTest {
 
     @Test
-    void getName() {
+    public void shouldIncreaseHungerLevelByFive() {
+        Dog underTest = new Dog("Max","Brown","Boxer",0,0,0,0,0);
+        underTest.feed();
+        assertEquals(5, 5);
+    }
+    @Test
+    public void shouldIncreaseThirstLevelByFive() {
+        Dog underTest = new Dog("Max","Brown","Boxer",0,0,0,0,0);
+        underTest.giveWater();
+        assertEquals(5, 5);
+    }
+    @Test
+    public void shouldIncreaseNeedsBathLevelByFive() {
+        Dog underTest = new Dog("Max","Brown","Boxer",0,0,0,0,0);
+        underTest.giveBath();
+        assertEquals(5, 5);
+    }
+    @Test
+    public void shouldDecreaseAllDogStatsByFive() {
+        Dog underTest = new Dog("Max","Brown","Boxer",10,10,10,10,10);
+        underTest.tick();
+        assertEquals(5, 5);
     }
 }
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -3,10 +3,11 @@ package virtual_pet;
 import org.junit.jupiter.api.Test;
 
 public class VirtualPetTest {
-    @Test
-    public void virtualPetHasAName(){
-        VirtualPet underTest = new VirtualPet("Rover");
+//    @Test
+//    public void virtualPetHasAName(){
+//        VirtualPet underTest = new VirtualPet("Rover");
+//
+//
+//    }
 
-
-    }
 }
